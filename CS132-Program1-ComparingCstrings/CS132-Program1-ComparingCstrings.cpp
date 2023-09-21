8e90e0515b57f8274072945fede98e50d0af8a1d
Taylor Bisset <taylorbisset@gmail.com>
2023-09-21 10:00:19 -0700

    Insert program header comments

--- CS132-Program1-ComparingCstrings/CS132-Program1-ComparingCstrings.cpp
+++ CS132-Program1-ComparingCstrings/CS132-Program1-ComparingCstrings.cpp
@@ -1,3 +1,12 @@
+////
+// Author:       Taylor Bisset 
+// Section:      A
+// Assignment:   P1
+// Description:  Without the use of cstring functions, 
+//               this program alphabatizes the given strings
+////
+
+
 #include <iostream>
 
 int main()
