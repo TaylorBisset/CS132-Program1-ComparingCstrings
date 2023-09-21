1d77c0daf88a327a6838711f8badff7164c7853b
Taylor Bisset <taylorbisset@gmail.com>
2023-09-21 10:06:24 -0700

    Insert pre-built code from assignment.

--- CS132-Program1-ComparingCstrings/CS132-Program1-ComparingCstrings.cpp
+++ CS132-Program1-ComparingCstrings/CS132-Program1-ComparingCstrings.cpp
@@ -6,12 +6,42 @@
 //               this program alphabatizes the given strings
 ////
 
-
 #include <iostream>
+
+using namespace std;
+
+/*
+if arg1 < arg2 returns a negative number
+if arg1 > arg2 returns a positive number
+if arg1 == arg2 returns zero
+*/
+
+int compareTo(const char* lstr, const char* rstr);  // needs to be written
+int length(const char* str);                        // needs to be written
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char str1[] = "batman";
+    char str2[] = "cat";
+    char str3[] = "cast";
+    char str4[] = "catagory";
+    char str5[] = "ZIP";
+
+    cout << "batman " << length(str1) << endl;
+    cout << "cat " << length(str2) << endl;
+
+    cout << "comparing " << str1 << " & " << str2 << " " << compareTo(str1, str2) << endl;
+    cout << "comparing " << str2 << " & " << str1 << " " << compareTo(str2, str1) << endl;
+
+    cout << "comparing " << str3 << " & " << str2 << " " << compareTo(str3, str2) << endl;
+    cout << "comparing " << str2 << " & " << str3 << " " << compareTo(str2, str3) << endl;
+
+    cout << "comparing " << str4 << " & " << str2 << " " << compareTo(str4, str2) << endl;
+    cout << "comparing " << str2 << " & " << str4 << " " << compareTo(str2, str4) << endl;
+
+    cout << "comparing " << str2 << " & " << str2 << " " << compareTo(str2, str2) << endl;
+
+    cout << "comparing " << str2 << " & " << str5 << " " << compareTo(str2, str5) << endl;
 
 #ifdef _WIN32
     system("pause");
